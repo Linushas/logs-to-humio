b2a7a7b6707540030368cae99986ae4db5c5f2f1
linus hasselkvist <linus.hasselkvist@schibsted.com>
2025-06-24 12:17:37 +0200

    changed default sink config to humio

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ CONFIG_DIR = "../config"
 OUTPUT_YAML = "../vector.yaml"
 CONSOLE_SINK = CONFIG_DIR+"/console_sink.yaml"
 HUMIO_SINK = CONFIG_DIR+"/sinks.yaml"
-SINK = CONSOLE_SINK
+SINK = HUMIO_SINK
